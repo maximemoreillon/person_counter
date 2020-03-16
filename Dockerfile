8f85b9c056735f4edb8a33bcc7f3dd7f2c8705fe
Maxime Moreillon <m.moreillon@gmail.com>
2020-03-16 08:30:43 +0000

    prepared deployment

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:latest
 WORKDIR /usr/src/app
 COPY . .
-RUN pip3 install
+RUN pip3 install -r requirements.txt
 EXPOSE 8051
 CMD [ "python3", "server.py" ]
--- server.py
+++ server.py
@@ -1,26 +1,29 @@
-import tensorflow as tf
-from flask import Flask
 import os
-import cv2 #temporary, remove once done
-import numpy as np # Temporary too I thik
+import tensorflow as tf
+from flask import Flask, request
+import numpy as np
 
-# Model doanload
+# Model download
 MODEL_NAME = 'ssd_mobilenet_v2_coco_2018_03_29'
 LOCAL_FILE_NAME = '{}.tar.gz'.format(MODEL_NAME)
 MODELS_DIRECTORY = '{}/models/'.format(os.getcwd())
 MODEL_URL = 'http://download.tensorflow.org/models/object_detection/{}.tar.gz'.format(MODEL_NAME)
 MODEL_PATH = '{}/models/{}/saved_model'.format(os.getcwd(), MODEL_NAME)
 
+print('Downloading model ...')
 model_dir = tf.keras.utils.get_file(
     fname=LOCAL_FILE_NAME,
     origin=MODEL_URL,
     cache_subdir=MODELS_DIRECTORY,
     archive_format='tar',
     extract=True)
+print('Downloading model OK')
 
 # Model loading
+print('Loading model ...')
 model = tf.saved_model.load(MODEL_PATH)
 model = model.signatures['serving_default']
+print('Loading model OK')
 
 app = Flask(__name__)
 
@@ -28,13 +31,20 @@ app = Flask(__name__)
 def hello_world():
     return 'Object detection microservice'
 
+
 @app.route('/predict', methods=['GET','POST']) # Change to only POST when done
 def predict():
 
-    # Work on the image
-    image = cv2.imread('./test_image.jpg')
-    image_np_expanded = np.expand_dims(image, axis=0)
-    image_tensor = tf.convert_to_tensor(image_np_expanded)
+    print('Starting count')
+
+    content = request.json
+    # This is super slow
+    image = eval('np.array({})'.format(content['instances']))
+
+    print(image.shape)
+
+    # conversion to tensor
+    image_tensor = tf.convert_to_tensor(image, dtype=np.uint8)
 
     # Feed the image to the network
     output_dict = model(image_tensor)
@@ -57,6 +67,7 @@ def predict():
 
     print("Person count: {}".format(person_count))
 
-    return "Person count: {}".format(person_count)
+    return {"count": person_count}
 
+print('Starting server')
 app.run(host='0.0.0.0', port=8051)
